e5b51581832e33216c1f0fa9d17185e9abb5ccad
Gergő Papp <gergo.papp-balint@oxit.hu>
2024-09-17 16:32:19 +0200

    added get_model()

---
+++ rtdetrv2/src/utils.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+
+from .core import YAMLConfig
+
+
+def get_model(args) -> nn.Module:
+    """"""
+    cfg = YAMLConfig(args)
+
+    if args.checkpoint:
+        checkpoint = torch.load(args.checkpoint, map_location="cpu")
+        if "ema" in checkpoint:
+            state = checkpoint["ema"]["module"]
+        else:
+            state = checkpoint["model"]
+    else:
+        raise AttributeError("Only support resume to load model.state_dict by now.")
+
+    cfg.model.load_state_dict(state)
+
+    class Model(nn.Module):
+        def __init__(
+            self,
+        ) -> None:
+            super().__init__()
+            self.model = cfg.model.deploy()
+            self.postprocessor = cfg.postprocessor.deploy()
+
+        def forward(self, images, orig_target_sizes):
+            outputs = self.model(images)
+            outputs = self.postprocessor(outputs, orig_target_sizes)
+            return outputs
+
+    model = Model().to(args.device)
+
+    return model
