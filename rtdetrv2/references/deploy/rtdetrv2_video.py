63b385b48d377a28ef62290caf4353358e954647
Gergő Papp <gergo.papp-balint@oxit.hu>
2024-10-30 16:39:26 +0100

    removed unnecessary files

--- rtdetrv2/references/deploy/rtdetrv2_video.py
+++ rtdetrv2/references/deploy/rtdetrv2_video.py
@@ -2,7 +2,7 @@
 """
 
 import torch
-import torch.nn as nn 
+import torch.nn as nn
 import torchvision.transforms as T
 import sys
 import os
@@ -12,62 +12,65 @@ sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "../
 from pathlib import Path
 import pickle
 import cv2
-import numpy as np 
+import numpy as np
 from PIL import Image
 
 from src.core import YAMLConfig
 
 color = [
-    (255, 255, 0), # Goal
-    (0, 0, 255), # Goalkeeper
-    (25, 255, 0), # Player
-    (0, 255, 255) # Referee
+    (255, 255, 0),
+    (0, 0, 255),
+    (25, 255, 0),
+    (0, 255, 255),
 ]
+
 
 def draw(frame, labels, boxes, scores):
     for i, bbox in enumerate(boxes):
-        cv2.rectangle(frame, bbox[:2], bbox[2:4], color[labels[i]-1], 2)
+        cv2.rectangle(frame, bbox[:2], bbox[2:4], color[labels[i] - 1], 2)
 
     img = cv2.resize(np.array(frame), (1280, 720))
-    cv2.imshow('out', img)
-
+    cv2.imshow("out", img)
 
 
 def main(args):
-    """main
-    """
+    """main"""
     cfg = YAMLConfig(args.config, exp_name=args.exp_name, resume=args.resume)
 
     if args.resume:
-        checkpoint = torch.load(args.resume, map_location='cpu') 
-        if 'ema' in checkpoint:
-            state = checkpoint['ema']['module']
+        checkpoint = torch.load(args.resume, map_location="cpu")
+        if "ema" in checkpoint:
+            state = checkpoint["ema"]["module"]
         else:
-            state = checkpoint['model']
+            state = checkpoint["model"]
     else:
-        raise AttributeError('Only support resume to load model.state_dict by now.')
+        raise AttributeError("Only support resume to load model.state_dict by now.")
 
     # NOTE load train mode state -> convert to deploy mode
     cfg.model.load_state_dict(state)
 
     class Model(nn.Module):
-        def __init__(self, ) -> None:
+        def __init__(
+            self,
+        ) -> None:
             super().__init__()
             self.model = cfg.model.deploy()
             self.postprocessor = cfg.postprocessor.deploy()
-            
+
         def forward(self, images, orig_target_sizes):
             outputs = self.model(images)
             outputs = self.postprocessor(outputs, orig_target_sizes)
-            
+
             return outputs
 
     model = Model().to(args.device)
-    transforms = T.Compose([
-        T.Resize((640, 640)),
-        T.ToTensor(),
-    ])
-    
+    transforms = T.Compose(
+        [
+            T.Resize((640, 640)),
+            T.ToTensor(),
+        ]
+    )
+
     video_cap = cv2.VideoCapture(args.im_file)
 
     bboxes = []
@@ -87,9 +90,8 @@ def main(args):
         bboxes.append(boxes)
         draw(frame, labels, boxes, scores)
 
-
         key = cv2.waitKey(10) & 0xFF
-        if key == 27 or key == ord('q') or key == ord('Q'):
+        if key == 27 or key == ord("q") or key == ord("Q"):
             break
 
     video_cap.release()
@@ -98,15 +100,16 @@ def main(args):
         pickle.dump(bboxes, f)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import argparse
     from datetime import datetime
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config', type=str, )
-    parser.add_argument('-r', '--resume', type=str, )
-    parser.add_argument('-f', '--im-file', type=str, )
-    parser.add_argument('-d', '--device', type=str, default='cpu')
-    parser.add_argument('-n', '--exp_name', type=str, default=datetime.now().strftime("%Y%m%d_%H%M%S"))
+    parser.add_argument("-c", "--config", type=str)
+    parser.add_argument("-r", "--resume", type=str)
+    parser.add_argument("-f", "--im-file", type=str)
+    parser.add_argument("-d", "--device", type=str, default="cpu")
+    parser.add_argument("-n", "--exp_name", type=str, default=datetime.now().strftime("%Y%m%d_%H%M%S"))
     args = parser.parse_args()
+
     main(args)
